1f853bafb84d56880914cb482d50903cc66b43b1
Pellegrin69 <pellegrinpierre@hotmail.fr>
2023-07-31 16:30:10 +0200

    Code cleaning

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,7 @@ import express from 'express';
 
 const userRouter = express.Router()
 
-import {getAllUsers, createUser, registerUser} from '../controllers/user.js';
+import {getAllUsers, registerUser} from '../controllers/user.js';
 import {loginUser} from "../controllers/auth.js";
 import {authMiddleware} from "../middleware/auth.js";
 
@@ -12,7 +12,6 @@ userRouter.post('/login', loginUser);
 
 // Routes protégées
 userRouter.get('/', authMiddleware, getAllUsers);
-userRouter.post('/', authMiddleware, createUser);
 
 
 export default userRouter;

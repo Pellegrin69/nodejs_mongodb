501860ede930ebe57cc6b620e4e31180f8fba6b7
Pellegrin69 <pellegrinpierre@hotmail.fr>
2023-07-31 16:29:30 +0200

    Code cleaning

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,5 +1,5 @@
 import User from '../models/user.js';
-import { validationResult } from 'express-validator';
+import {validationResult} from 'express-validator';
 import bcrypt from 'bcryptjs';
 
 
@@ -8,28 +8,28 @@ export const registerUser = async (req, res) => {
     // Vérifier les erreurs de validation
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
+        return res.status(422).json({errors: errors.array()});
     }
 
-    const { name, email, password } = req.body;
+    const {name, email, password} = req.body;
 
     try {
         // Vérifier si l'utilisateur existe déjà
-        const existingUser = await User.findOne({ email });
+        const existingUser = await User.findOne({email});
         if (existingUser) {
-            return res.status(409).json({ message: 'L\'utilisateur existe déjà.' });
+            return res.status(409).json({message: 'L\'utilisateur existe déjà.'});
         }
 
         // Hacher le mot de passe avant de le stocker dans la base de données
         const hashedPassword = await bcrypt.hash(password, 12);
 
         // Créer un nouvel utilisateur dans la base de données
-        const newUser = new User({ name, email, password: hashedPassword });
+        const newUser = new User({name, email, password: hashedPassword});
         await newUser.save();
 
-        res.status(201).json({ message: 'Inscription réussie.'  });
+        res.status(201).json({message: 'Inscription réussie.'});
     } catch (error) {
-        res.status(500).json({ error: error.message });
+        res.status(500).json({error: error.message});
     }
 };
 
@@ -38,25 +38,9 @@ export const getAllUsers = async (req, res) => {
     try {
         const users = await User.find();
         const usersDTO = users.map(user => {
-            return { name: user.name, email: user.email };
+            return {name: user.name, email: user.email};
         });
         res.status(200).json(usersDTO);
-    } catch (err) {
-        res.status(500).json({error: err.message});
-    }
-};
-
-// Créer un utilisateur
-export const createUser = async (req, res) => {
-    const {name} = req.body;
-
-    try {
-        const newUser = new User({name});
-        await newUser.save();
-        res.status(201).json({
-            message: 'User successfully created!',
-            user: newUser,
-        });
     } catch (err) {
         res.status(500).json({error: err.message});
     }

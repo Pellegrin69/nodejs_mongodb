2bd679939a6b5822f4f696e860d5ed9c9c15e9a1
Pellegrin69 <pellegrinpierre@hotmail.fr>
2023-07-31 16:21:11 +0200

    Error handling in log

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import morgan from 'morgan';
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
 import * as fs from "fs";
-import * as path from "path";
+import { fileURLToPath } from 'url';
 
 // Charger les variables d'environnement à partir du fichier .env
 dotenv.config();
@@ -16,17 +16,37 @@ const app = express();
 app.use(bodyParser.json()); // Parser les requêtes JSON
 app.use(bodyParser.urlencoded({extended: false})); // Parser les requêtes URL-encoded
 
-// Créer un fichier de log pour les erreurs
-const errorLogStream = fs.createWriteStream(path.join(__dirname, 'error.log'), {flags: 'a'});
-
-// Utiliser le middleware de journalisation "morgan" avec une configuration personnalisée
-app.use(morgan('combined', {stream: errorLogStream}));
-
 
 // Import des routeurs
 import userRouter from './routes/user.js';
-
 app.use('/api/users', userRouter);
+
+// Obtenir le chemin du fichier de log
+const logFilePath = new URL('error.log', import.meta.url);
+const filePath = fileURLToPath(logFilePath);
+const errorLogStream = fs.createWriteStream(filePath, { flags: 'a' });
+// Utiliser le middleware de journalisation "morgan" avec une configuration personnalisée
+app.use(morgan('combined', {stream: errorLogStream}));
+
+// Gestionnaire d'erreurs personnalisé pour rediriger les erreurs vers le middleware de journalisation
+app.use((err, req, res, next) => {
+    // Vérifiez s'il y a une erreur dans le traitement de la requête
+    if (err) {
+        // Enregistrez l'erreur dans le middleware de journalisation (morgan)
+        const errorMessage = `[${new Date().toISOString()}] ${err.stack || err.message}\n`;
+        errorLogStream.write(errorMessage);
+
+        // Ne renvoyez pas l'erreur au client ici pour éviter un double envoi
+        return;
+    }
+    // Continuez à traiter l'erreur pour renvoyer une réponse appropriée au client
+    next(err);
+});
+
+// Dernier middleware pour gérer les erreurs non capturées
+app.use((err, req, res, next) => {
+    res.status(500).json({ error: 'Une erreur est survenue lors du traitement de votre requête.' });
+});
 
 mongoose.connect(process.env.MONGO_CONNECTION_STRING)
     .then(() => {
